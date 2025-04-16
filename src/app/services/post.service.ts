e832690029104732e8d94281ed2490ea221ba505
Ovidiu Carlan <ovidiu.18.carlan@gmail.com>
2025-04-16 21:20:01 +0200

    Post Service
    Post Service

---
+++ src/app/services/post.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { PostData } from '../../shared/models/post-model';
+import { Observable, catchError, throwError } from 'rxjs';
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PostService {
+
+  readonly url = 'http://localhost:8080';
+
+  constructor(private http: HttpClient, ) { }
+
+  createPost(data: PostData): Observable<PostData> {
+    return this.http.post<PostData>(this.url + '/posts', data)
+      .pipe(
+        catchError((error: HttpErrorResponse) => {
+          console.error('Error creating post:', error);
+          return throwError(() => error);
+        })
+      );
+  }
+
+  getPosts(): Observable<PostData[]> {
+    return this.http.get<PostData[]>(this.url + '/posts');
+  }
+}
